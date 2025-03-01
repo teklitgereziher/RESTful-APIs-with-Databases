4965ef8f7ede9f444385c594c5af9bdddbacd9c1
TeklitB <teklitber@gmail.com>
2025-03-01 11:53:43 +0100

    add address to publisher

--- Cosmosdb-CRUD-Web-API/src/AzureCosmos.CRUD.DataAccess/Models/Publisher.cs
+++ Cosmosdb-CRUD-Web-API/src/AzureCosmos.CRUD.DataAccess/Models/Publisher.cs
@@ -7,6 +7,6 @@ namespace CRUD_Operation_DataAccess.Models
     public string Email { get; set; }
     public string Website { get; set; }
 
-    public string Address { get; set; }
+    public Address Address { get; set; }
   }
 }
